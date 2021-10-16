d1c777186f04b6554956d1b23ac6334af4037287
ohad24333 <58825663+ohad24333@users.noreply.github.com>
2021-10-16 20:11:52 +0300

    Add files via upload

---
+++ Events1/Event1/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace Events_UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.btnEnterNumber = new System.Windows.Forms.Button();
+            this.gridStudents = new System.Windows.Forms.DataGridView();
+            this.btnAddStudent = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAddStudent = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gridStudents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(32, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 21);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Enter Number:";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(174, 79);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(123, 23);
+            this.txtNumber.TabIndex = 3;
+            // 
+            // btnEnterNumber
+            // 
+            this.btnEnterNumber.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnEnterNumber.Location = new System.Drawing.Point(303, 80);
+            this.btnEnterNumber.Name = "btnEnterNumber";
+            this.btnEnterNumber.Size = new System.Drawing.Size(75, 24);
+            this.btnEnterNumber.TabIndex = 4;
+            this.btnEnterNumber.Text = "Enter";
+            this.btnEnterNumber.UseVisualStyleBackColor = true;
+            this.btnEnterNumber.Click += new System.EventHandler(this.btnEnterNumber_Click);
+            // 
+            // gridStudents
+            // 
+            this.gridStudents.BackgroundColor = System.Drawing.Color.LightGray;
+            this.gridStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridStudents.Location = new System.Drawing.Point(32, 127);
+            this.gridStudents.Name = "gridStudents";
+            this.gridStudents.RowTemplate.Height = 25;
+            this.gridStudents.Size = new System.Drawing.Size(346, 311);
+            this.gridStudents.TabIndex = 5;
+            // 
+            // btnAddStudent
+            // 
+            this.btnAddStudent.Location = new System.Drawing.Point(303, 36);
+            this.btnAddStudent.Name = "btnAddStudent";
+            this.btnAddStudent.Size = new System.Drawing.Size(75, 23);
+            this.btnAddStudent.TabIndex = 6;
+            this.btnAddStudent.Text = "Add Student";
+            this.btnAddStudent.UseVisualStyleBackColor = true;
+            this.btnAddStudent.Click += new System.EventHandler(this.btnAddStudent_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(32, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 21);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Add Student:";
+            // 
+            // txtAddStudent
+            // 
+            this.txtAddStudent.Location = new System.Drawing.Point(174, 36);
+            this.txtAddStudent.Name = "txtAddStudent";
+            this.txtAddStudent.Size = new System.Drawing.Size(123, 23);
+            this.txtAddStudent.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 471);
+            this.Controls.Add(this.txtAddStudent);
+            this.Controls.Add(this.btnAddStudent);
+            this.Controls.Add(this.gridStudents);
+            this.Controls.Add(this.btnEnterNumber);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gridStudents)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Button btnEnterNumber;
+        private System.Windows.Forms.DataGridView gridStudents;
+        private System.Windows.Forms.Button btnAddStudent;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAddStudent;
+    }
+}
+
---
+++ Events1/Event1/Form1.cs
@@ -0,0 +1,72 @@
+﻿
+using Events1.Librery;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Events_UI
+{
+    public partial class Form1 : Form
+    {
+        public delegate void PrintDelegate(string str);
+
+        public Form1()
+        {
+
+            StudentManager.FiveStudentAdded += PrintMessegeBox;
+            Student.TooLongNameEvent += PrintByMessegeBox;
+            Student.LuckyNumberEvent += PrintByMessegeBox;
+            InitializeComponent();
+
+            BindingSource bs = new BindingSource();
+            bs.DataSource =  StudentManager.GetList();
+            gridStudents.DataSource = bs;
+        }
+
+        private void PrintMessegeBox()
+        {
+            MessageBox.Show("You Get 5% Discount!");
+        }
+
+ 
+        public static void PrintByMessegeBox(string str)
+        {
+            MessageBox.Show(str);
+        }
+
+        public static void PrintByMessegeBox(object ob , TooLongNameEventArgs e)
+        {
+            MessageBox.Show(e.Messege);
+        }
+
+        public static void PrintByMessegeBox(object ob, LuckyNumberEventArgs e)
+        {      
+            MessageBox.Show(e.Messege);
+        }
+
+
+        private void btnEnterNumber_Click(object sender, EventArgs e)
+        {
+            Student student = new Student() { Number = txtNumber.Text };
+           
+        }
+
+        private void btnAddStudent_Click(object sender, EventArgs e)
+        {
+
+            StudentManager.Add(new Student() { FirstName = txtAddStudent.Text, Number = txtNumber.Text });
+            BindingSource bs = new BindingSource();
+            bs.DataSource = StudentManager.GetList();
+            gridStudents.DataSource = bs;
+            
+        }
+
+        
+    }
+}
